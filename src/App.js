2a2ec2dd903863141527b7a771e0f5603ccbf5a5
Enigma-cloud <torreskristian77@gmail.com>
2021-08-23 22:35:49 +0100

    f: added some page routing & used withRouter to avoid prop drilling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+
 function App() {
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </>
   );
 }
