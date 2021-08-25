a53ba32341276c8bed3e18ffec80dd3fc492df6a
Enigma-cloud <torreskristian77@gmail.com>
2021-08-25 19:11:06 +0100

    fs: added sign in page + custom inputs and button components

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import './sign-in.styles.scss';
+
+class SignIn extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        this.setState({ email: '', password: ''})
+    }
+
+    handleChange = (e) => {
+        const { value, name } = e.target;
+
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return (
+            <div className='sign-in'>
+                <h2 className='title'>I already have an account</h2>
+                <span>Sign in with your email and password</span>
+                            
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput type="email" name="email" label="Email" handleChange={this.handleChange} value={this.state.email} required />
+                    <FormInput type="password" name="password" label="Password" handleChange={this.handleChange} value={this.state.password} required />
+                    <CustomButton type="submit">Sign in</CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
