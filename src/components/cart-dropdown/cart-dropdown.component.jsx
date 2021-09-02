a8f5692e42e9a2dfff81838d3809cba51d209de8
Enigma-cloud <torreskristian77@gmail.com>
2021-09-02 13:49:28 +0100

    fs: added Cart-Item components into Cart-Dropdown component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItemsProp }) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+            {
+                cartItemsProp.map((cartItem) => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))
+            }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProsp = ({ cart: { cartItems }}) => ({
+    cartItemsProp: cartItems
+}); 
+
+export default connect(mapStateToProsp)(CartDropdown);
